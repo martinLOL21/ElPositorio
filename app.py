7195c60f6b30fdf3a02d3d3a32175d380cc78484
martinLOL21 <52983705+martinLOL21@users.noreply.github.com>
2026-08-20 10:17:53 -0500

    Add two-column layout with checkbox interaction

--- app.py
+++ app.py
@@ -11,3 +11,14 @@ st.image(image, caption="interfaces multimodales")
 
 texto = st.text_input("Escribe algo", "este es mi texto")
 st.write("El texto escrito es", texto)
+
+st.subheader("Ahora usemos 2 Columnas")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+  resp = st.checkbox("Estoy de acuerdo")
+  if resp:
+    st.write("Correcto!")
