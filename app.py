56f2aaa043d27225253ccc57cb016a38436c5c26
martinLOL21 <52983705+martinLOL21@users.noreply.github.com>
2026-08-20 10:22:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,3 +22,14 @@ with col1:
   resp = st.checkbox("Estoy de acuerdo")
   if resp:
     st.write("Correcto!")
+
+with col2:
+  st.subheader("Esta es la segunda columna")
+  modo = st.radio("Que Modalidad es la principal en tu interfaz", ("Visual", "Auditivo", "Tactil"))
+  if modo "" "Visual":
+    st.write("La vista es fundamental para tu interfaz")
+   if modo "" "Auditivo":
+    st.write("El audio es fundamental para tu interfaz")
+   if modo "" "Tactil":
+    st.write("El tacto es fundamental para tu interfaz")
+  
