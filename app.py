4cbb2adb1db451582c83fd1ebc3512d6ee9d4a55
martinLOL21 <52983705+martinLOL21@users.noreply.github.com>
2026-08-20 10:03:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.title( " Hola!!! mi nombre es Martilin " )
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales")
+st.write ("Facilmente puedo realizar el backend y frontend")
+image = Image.open("Foto.jpg")
+st.image(image, caption="interfaces multimodales")
